a647b60e566fd40ea27de09e0b1ebecd193ae7db
LZYBJB <1415790333@qq.com>
2025-07-17 21:13:32 +0800

    单例基类

---
+++ Assets/KC_Scripts/Common/SingletonBase/KCSingleton.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+namespace KCGame
+{
+    public class KCSingleton<T> where T : new()
+    {
+        
+        private static readonly object _lock = new object();
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new T();
+                            (_instance as KCSingleton<T>)?.OnCreated(); // 延迟初始化
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        // 让构造函数为 protected 避免外部直接实例化
+        protected KCSingleton() {}
+
+        protected virtual void OnCreated() { Debug.Log($"Instance of type {typeof(T).Name} created."); }
+        public virtual void WakeUp()
+        {
+
+        }
+       
+    }
+}
---
+++ Assets/KC_Scripts/Common/SingletonBase/KCSingletonMonoBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+
+namespace KCGame
+{
+    public class KCSingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+
+        private static T _instance;
+        private static readonly object _lock = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        _instance = FindObjectOfType<T>();
+
+                        if (_instance == null)
+                        {
+                            GameObject singletonObject = new GameObject(typeof(T).Name);
+                            _instance = singletonObject.AddComponent<T>();
+                            DontDestroyOnLoad(singletonObject);
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+
+        public static InstanceType GetTypeInstance<InstanceType>() where InstanceType : MonoBehaviour
+        {
+            return Instance as InstanceType;
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                if (transform.parent == null)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+            else if (_instance != this)
+            {
+                Debug.LogWarning($"实例已存在，销毁多余的 {GetType().Name}");
+                Destroy(gameObject);
+            }
+
+            Init();
+        }
+
+        protected virtual void Init()
+        {
+
+        }
+
+    }
+}
