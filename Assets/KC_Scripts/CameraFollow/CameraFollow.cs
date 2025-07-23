3585d2c9a5f2391c7f0e44699b876af63bb80ed5
LZYBJB <1415790333@qq.com>
2025-07-23 23:17:55 +0800

    重写相机

--- Assets/KC_Scripts/CameraFollow/CameraFollow.cs
+++ Assets/KC_Scripts/CameraFollow/CameraFollow.cs
@@ -1,40 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 namespace KCGame
 {
 
-    /* 
-     * 描述：相机跟随人物移动
-     * 作者：sine5RAD
-     */
     public class CameraFollow : MonoBehaviour
     {
-        public Transform target;
-        public float smoothing;
-        // Start is called before the first frame update
-        void Start()
+
+
+        public CinemachineVirtualCamera VirtualCamera;
+
+        public void Start()
         {
+            Transform tr_Player = GameObject.FindWithTag("Player").transform;
+            VirtualCamera.Follow = tr_Player;
+            VirtualCamera.LookAt = tr_Player;
+
         }
 
-        private void LateUpdate()
-        {
-            if (target != null)
-            {
-                if (transform.position != target.position)
-                {
-                    Vector3 targetPos = target.position;
-                    transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
-                }
-            }
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 
 }
